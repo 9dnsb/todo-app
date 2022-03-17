1660f85ccd4e5df3b2b4fd15784c2f39b65ce147
David Blatt <davidblatt10@gmail.com>
2022-03-17 10:14:31 -0400

    small about page text changes

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -12,8 +12,8 @@ function About() {
           <TitleFormField aTitle="About" component="h1" />
           <Typography variant="body1" gutterBottom>
             This website is a simple Todo project. Its my first attempt creating
-            a website using the MERN stack. Its also my first time using
-            Material UI.
+            a website using the MERN stack. It uses Redux to manage state. Its
+            also my first time using Material UI.
           </Typography>
           <Typography variant="body1" gutterBottom>
             I spent a lot of time refactoring the website and have tried to
